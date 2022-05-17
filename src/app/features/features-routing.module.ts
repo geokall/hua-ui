1f91289cd5820b26705e63132ba9f3db70d8fd1b
geokall <gkallimanis@iknowhow.com>
2022-05-17 16:42:49 +0300

    features and features-routing modules

---
+++ src/app/features/features-routing.module.ts
@@ -0,0 +1,34 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [
+  // {
+  //   path: 'decisions',
+  //   canActivate: [AuthGuard],
+  //   children: [
+  //     {
+  //       path: 'create',
+  //       component: DecisionFormComponent,
+  //       canActivate: [AuthGuard, GeneralGuard]
+  //     },
+  //     {path: 'edit/:id', component: DecisionFormComponent, canActivate: [AuthGuard, GeneralGuard]},
+  //     {
+  //       path: 'search',
+  //       component: DecisionsSearchComponent
+  //     },
+  //     {path: 'preview/:id', component: PreviewDocumentComponent},
+  //   ]
+  // },
+  // {path: 'users-management', component: UserManagementComponent, canActivate: [AuthGuard, AdminGuard]},
+  // {path: '', component: DecisionsSearchComponent, canActivate: [AuthGuard]},
+  // {path: 'login', component: LoginComponent},
+  // {path: 'taxis_login_callback', component: TaxisLoginComponent},
+  // {path: '**', redirectTo: ''}
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class FeaturesRoutingModule {
+}
