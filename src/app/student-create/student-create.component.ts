365c5267402396f424069b635da04daabf00151a
geokall <gkallimanis@iknowhow.com>
2022-05-19 12:48:17 +0300

    strictPropertyInitialization : false Added student create first step

--- src/app/student-create/student-create.component.ts
+++ src/app/student-create/student-create.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-student-create',
@@ -7,10 +8,43 @@ import {Component, OnInit} from '@angular/core';
 })
 export class StudentCreateComponent implements OnInit {
 
+  form: FormGroup;
+  saving: boolean = false;
+  loading: boolean = false;
+  successModal: boolean = false;
+  errorModal: boolean = false;
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm(): void {
+    this.form = new FormGroup({
+      id: new FormControl(null),
+      studentName: new FormControl(null, Validators.required),
+      formation: new FormControl(null, Validators.required),
+      documentType: new FormControl(null, Validators.required),
+      decisionYear: new FormControl(null, Validators.required),
+
+    });
+  }
+
+  onClear(): void {
+    // this.form.reset(this.form); //not sure
+  }
+
+  onSubmit(): void {
+    // this.saving = true;
+    // const form = this.form.value as any; //as student form
+    // this.form.reset(form);
+    // this.decisionFormService
+    //   .save(form)
+    //   .pipe(first())
+    //   .subscribe((res: HttpResponse<any>) => this.successModal = true, (error: HttpErrorResponse) => this.errorModal = true)
+    //   .add(() => this.saving = false);
   }
 
 }
