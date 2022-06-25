d840b63f2da2bb9addb15556f7c76c5a42c6f3bb
geokall <gkallimanis@iknowhow.com>
2022-06-25 14:52:08 +0300

    added server url on prod and changed budgets on angular.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
-  allowedDomains: ['localhost:4200', 'localhost:8080','localhost'],
+  allowedDomains: ['localhost:4200', 'localhost:8080', 'localhost'],
   disallowedDomains: ['http://localhost:4200/auth/login', 'http://localhost:8080/auth/login'],
   solrUrl: '/solr',
   apiUrl: '',
+  serverUrl: 'http://localhost:8080',
   debug: false
 };
