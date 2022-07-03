ae12ce405c5f39bee774f8d49a34f83c6c8eb088
ba055482 <ba055482>
2022-07-03 10:55:47 +0000

    add https on allowedDomains

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  allowedDomains: ['huademo.gotdns.ch/api'],
+  allowedDomains: ['https://huademo.gotdns.ch/api'],
   disallowedDomains: ['https://huademo.gotdns.ch/api/auth/login'],
   serverUrl: 'https://huademo.gotdns.ch/api',
   debug: false
